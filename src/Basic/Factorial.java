3e3e102321b2a86229d286a554b6760c470814d9
Vinodhv11 <vvinodhv11@gmail.com>
2019-08-22 08:34:59 +0530

    new message for factorial

---
+++ src/Basic/Factorial.java
@@ -0,0 +1,23 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = sc.nextInt();
+		int fact = 1;
+		if (num<=0) {
+			fact=1;
+			
+		}
+		else {
+			for (int i = 1; i <= num; i++) {
+				fact = fact*i;
+				
+			}
+			System.out.println("The factorial is" + fact);
+		}
+	}
+}
